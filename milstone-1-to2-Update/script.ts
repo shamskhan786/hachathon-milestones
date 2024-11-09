c0f7bea8a4854a2270e1503ed50e364f752a9db8
shamskhan786 <165338143+shamskhan786@users.noreply.github.com>
2024-11-09 11:49:04 +0500

    Milstone 1 to 2 Update

---
+++ milstone-1-to2-Update/script.ts
@@ -0,0 +1,8 @@
+const button = document.getElementById(`button-skills`) as HTMLButtonElement;
+const skill = document.getElementById(`skills`) as HTMLElement;
+
+button.addEventListener(`click`,()=>{
+ if(skill.style.display === `none`){
+   skill.style.display = `block`
+ }else(skill.style.display = `none`)
+})
